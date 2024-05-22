26a05dbf53a5196fc156d95f17dfb1004c2dbddf
Eyal Gonen <eyal.gonen@miac.com>
2024-05-22 08:51:44 +0300

    Have 99-99_delete_all_repositories_ecr.sh source 00_vars.sh Fix README to include indication of prerequisite "jq" for 99_delete_all_repositories_ecr.sh

--- 99_delete_all_repositories_ecr.sh
+++ 99_delete_all_repositories_ecr.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-#
-# This script can be executed several times if for some unknown reason it does not delete all in 1 go
-#
-# PREREQUISITES:
-# 1. AWS CLI
-# 2. jq
-#
+source 00_vars.sh
 
-AWS_CLI_PARMS=
-
-## examples of additional parameters passed in to AWS CLI
-# AWS_CLI_PARMS=--profile default
-# AWS_CLI_PARMS=--region il-central-1
 
 # loop on all ECR repos and delete them
 for repName in $(aws ecr describe-repositories $AWS_CLI_PARMS --no-cli-pager  | jq -r '.repositories[].repositoryName')
